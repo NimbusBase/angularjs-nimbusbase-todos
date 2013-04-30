6cc623ec3fa176145a3580885cc06d2240feece4
Carl Lee <ljbha007@gmail.com>
2013-04-30 16:26:16 +0800

    Add auth secrets

--- js/app.js
+++ js/app.js
@@ -8,3 +8,16 @@
  * @type {angular.Module}
  */
 var todomvc = angular.module('todomvc', []);
+var sync_object = {
+	"GDrive": {
+		"key": "106903753207.apps.googleusercontent.com",
+		"scope": "https://www.googleapis.com/auth/drive",
+		"app_name": "nimbus-todo-demo"
+	},
+	"Dropbox": {
+		"key": "q5yx30gr8mcvq4f",
+		"secret": "qy64qphr70lwui5",
+		"app_name": "todomvc_app"
+	}
+};
+Nimbus.Auth.setup(sync_object);
