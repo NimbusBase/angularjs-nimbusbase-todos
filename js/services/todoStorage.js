fea8ad0bb7df1cdbcda2758aba1cef534b1fbb52
winterlightning <raycornellwang@gmail.com>
2013-05-08 15:08:11 -0400

    deleted useless old storage

--- js/services/todoStorage.js
+++ js/services/todoStorage.js
@@ -4,16 +4,4 @@
 /**
  * Services that persists and retrieves TODOs from localStorage
  */
-todomvc.factory('todoStorage', function () {
-	var STORAGE_ID = 'todos-angularjs';
-
-	return {
-		get: function () {
-			return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
-		},
-
-		put: function (todos) {
-			localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
-		}
-	};
-}).factory('nimbusbaseStorage', Nimbus.angularService);
+todomvc.factory('nimbusbaseStorage', Nimbus.angularService);
